3875ebd1760082c81ee48e2c37cc250991a75e0c
maxiDroiid <andreeminecrafter@gmail.com>
2022-04-20 22:20:33 -0500

    BasicSystem

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const config = require('./config/config.json')
+const fs = require('fs');
+require('colors')
+const client = new Discord.Client({
+    restTimeOffset: 0,
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    ],
+})
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.color = config.color;
+
+/* SISTEMA DE IDIOMAS */
+client.la = {};
+let idiomas = fs.readdirSync('./idiomas').filter(archivo => archivo.endsWith(".json")).map(idioma => idioma.replace(/.json/, ""));
+console.log(idiomas)
+for(const idioma of idiomas){
+    client.la[idioma] = require(`./idiomas/${idioma}`)
+}
+Object.freeze(client.la)
+
+function requerirhandlers() {
+    ["command", "events", "distube", "reaccion_roles", "tickets", "sugerencias", "sorteos", "bienvenida"].forEach(handler => {
+        try {
+            require(`./handlers/${handler}`)(client, Discord)
+        } catch (e) {
+            console.warn(e)
+        }
+    })
+}
+requerirhandlers();
+
+client.login(config.token).catch(() => console.log(`-[X]- NO HAS ESPECIFICADO UN TOKEN VALIDO O TE FALTAN INTENTOS -[X]-\n [-] ACTIVA LOS INTENTOS EN https://discord.dev [-]`.red))
+
+/*
+╔═════════════════════════════════════════════════════╗
+║    || - || Desarollado por dewstouh#1088 || - ||    ║
+║    ----------| discord.gg/MBPsvcphGf |----------    ║
+╚═════════════════════════════════════════════════════╝
+*/
