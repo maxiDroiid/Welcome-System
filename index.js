dd347dce580e02f0433798c5c7a2de6165669731
iMaxiDroiiid <36526861+maxiDroiid@users.noreply.github.com>
2022-04-20 22:55:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
 const Discord = require('discord.js');
-const config = require('./config/config.json')
-const fs = require('fs');
-require('colors')
+const config = require('./config/config.json');
+const Enmap = require('enmap');
+
 const client = new Discord.Client({
-    restTimeOffset: 0,
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
     intents: [
         Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_MEMBERS,
         Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    ],
+    ]
 })
 
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-client.color = config.color;
+client.login(config.token)
 
-/* SISTEMA DE IDIOMAS */
-client.la = {};
-let idiomas = fs.readdirSync('./idiomas').filter(archivo => archivo.endsWith(".json")).map(idioma => idioma.replace(/.json/, ""));
-console.log(idiomas)
-for(const idioma of idiomas){
-    client.la[idioma] = require(`./idiomas/${idioma}`)
-}
-Object.freeze(client.la)
+client.on("ready", () => {
+    console.log(`Conectado como ${client.user.tag}`)
+});
 
-function requerirhandlers() {
-    ["command", "events", "distube", "reaccion_roles", "tickets", "sugerencias", "sorteos", "bienvenida"].forEach(handler => {
-        try {
-            require(`./handlers/${handler}`)(client, Discord)
-        } catch (e) {
-            console.warn(e)
+client.setups = new Enmap({
+    name: "setups",
+    dataDir: "./databases"
+})
+
+client.on("messageCreate", async (message) => {
+    if (message.author.bot || !message.guild || !message.channel) return;
+    client.setups.ensure(message.guild.id, {
+        welcomechannel: "",
+        welcomemessage: "",
+    });
+    const args = message.content.slice(config.prefix.length).trim().split(" ")
+    const command = args.shift()?.toLowerCase();
+
+    if (command == "ping") {
+        return message.reply(`El ping del bot es de \`${client.ws.ping}ms\``)
+    }
+
+    if (command == "setup-welcome") {
+        const channel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first();
+        if (!channel) return message.reply(`El CANAL que has mencionado NO EXISTE!\n**Uso:** \`${config.prefix}setup-welcome <#CANAL O ID> <MENSAJE DE BIENVENIDA>\``);
+        if (!args.slice(1).join(" ")) return message.reply(`No has especificado el MENSAJE DE BIENVENIDA!\n**Uso:** \`${config.prefix}setup-welcome <#CANAL O ID> <MENSAJE DE BIENVENIDA>\``);
+        let obj = {
+            welcomechannel: channel.id,
+            welcomemessage: args.slice(1).join(" "),
+
         }
-    })
-}
-requerirhandlers();
+        client.setups.set(message.guild.id, obj)
+        return message.reply(`✅ Se ha configurado correctamente el canal de bienvenida\n**Canal:** ${channel}\n**Mensaje de Bienvenida:** ${args.slice(1).join(" ")}`)
+    }
+})
 
-client.login(config.token).catch(() => console.log(`-[X]- NO HAS ESPECIFICADO UN TOKEN VALIDO O TE FALTAN INTENTOS -[X]-\n [-] ACTIVA LOS INTENTOS EN https://discord.dev [-]`.red))
+client.on("guildMemberAdd", async (member) => {
+    client.setups.ensure(member.guild.id, {
+        welcomechannel: "",
+        welcomemessage: "",
+    });
 
-
+    try {
+        const data = client.setups.get(member.guild.id);
+        if(data) {
+            if(member.guild.channels.cache.get(data.welcomechannel)){
+                const channel = member.guild.channels.cache.get(data.welcomechannel)
+                const attachment = new Discord.MessageAttachment('https://i.imgur.com/wGO8KCd.png')
+                channel.send({content: data.welcomemessage.replace(/{usuario}/, member), files: [attachment]})
+            }
+        }
+    } catch (e){console.log(e)}
+})
